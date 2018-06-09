b3ed493c7039e9c08848e3cc41b877455875292d
student4 <student4@pcoip.ki.agh.edu.pl>
2018-06-09 12:50:25 +0200

    Add project files.

---
+++ PPiatekHelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PPiatekHelloWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
